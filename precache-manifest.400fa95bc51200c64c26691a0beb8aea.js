1b0b356f9e734c4950d33798a42aa6968ea3771b
Bruno Lourenco <brunnolou@gmail.com>
2019-03-26 21:00:14 +0000

    Updates

---
+++ precache-manifest.400fa95bc51200c64c26691a0beb8aea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c7c2dd234395ff45d93",
+    "url": "/pixel-perfect-grid/static/js/runtime~main.2c7c2dd2.js"
+  },
+  {
+    "revision": "b5ecd3c0c0617c83fbe7",
+    "url": "/pixel-perfect-grid/static/js/main.b5ecd3c0.chunk.js"
+  },
+  {
+    "revision": "324694a5124312be0d8f",
+    "url": "/pixel-perfect-grid/static/js/1.324694a5.chunk.js"
+  },
+  {
+    "revision": "b5ecd3c0c0617c83fbe7",
+    "url": "/pixel-perfect-grid/static/css/main.517d42d1.chunk.css"
+  },
+  {
+    "revision": "ab10196cd2da986577dd84e307a8b207",
+    "url": "/pixel-perfect-grid/index.html"
+  }
+];
